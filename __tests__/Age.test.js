9b407e1c8c988eaa89866708711178dc17743ed9
Tyler Sinks <tyler.sinks@gmail.com>
2021-04-14 18:29:44 -0400

    pass first world expect adds 7 spec

--- __tests__/Age.test.js
+++ __tests__/Age.test.js
@@ -69,7 +69,7 @@ describe("Age", () => {
   test("should return lifeExpect value increased by 7 when person is from first world country but NOT in the Americas", () => {
     newAge.region = "first world";
     newAge.calcLifeExpectance();
-    expect(newAge.lifeExpect).toEqual(80);
+    expect(newAge["first world"]).toEqual(80);
   });
 
 //   test("should return lifeExpect value decreased by 10 when African country is indicated", () => {
