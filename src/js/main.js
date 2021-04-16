13f357fda45e4c81c26c221f1c41afd051e852ab
Tyler Sinks <tyler.sinks@gmail.com>
2021-04-15 21:05:14 -0400

    add UI logic stub

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
 import Age from './Age.js';
 
+// this.yearsBeyond is assigned a non-zero once .calcLifeExpectance has been called. If this.yearsBeyond is negative, then these values indicate the person has lived x years longer than their calculated life expectancy.
+$('form').on("submit", function(event) {
+  event.preventPrefault();
+  const ageInput = parseInt($('#user-age-earth-years').val());
+  const inputRegion = $("")
+  const earthling = new Age(ageInput);
+  earthling.convertAll();
+
+});
